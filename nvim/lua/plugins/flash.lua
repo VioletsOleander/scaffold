832e500741bb805383fbf9eedf5ad336015aaa8e
VioletsOleander <1377232072@qq.com>
2026-02-28 22:32:42 +0800

    nvim: disable flash.nvim char mode, enable search mode by default

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -2,7 +2,16 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+        modes = {
+            search = {
+                enabled = true,
+            },
+            char = {
+                enabled = false,
+            },
+        },
+    },
     keys = {
         { "s",         mode = { "n", "o" },  function() require("flash").jump() end,              desc = "Flash" },
         { "S",         mode = { "n", "o" },  function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
