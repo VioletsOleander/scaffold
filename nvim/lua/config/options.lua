091e39dbe9f4f65c40ac1c76bd30e6b147880381
VioletsOleander <1377232072@qq.com>
2026-03-13 09:32:19 +0800

    nvim(option): diable some providers to speedup startup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,6 +8,11 @@ g.maplocalleader = "\\"
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_node_provider = 0
+
 -- Options
 -- timeout
 opt.timeoutlen = 300
