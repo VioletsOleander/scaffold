629ac748c91cac7cd4bf47e3088879b705b779ff
VioletsOleander <1377232072@qq.com>
2026-03-26 09:17:08 +0800

    nvim(plugin/ui): use onedarkpro with customized color

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,19 +1,29 @@
 return {
 	-- Colorscheme
 	{
-		"navarasu/onedark.nvim",
+		"olimorris/onedarkpro.nvim",
 		cond = not vim.g.vscode,
 		priority = 1000,
 		config = function()
-			require("onedark").setup({
-				style = "light",
-				highlights = {
-					Cursor = { fg = "#526fff", bg = "#ffffff" },
-				},
-			})
-			require("onedark").load()
+			local custom_colors = {
+				fg = "#2A2C33",
+				bg = "#fafafa",
+				black = "#000000",
+				blue = "#1c47cf",
+				cyan = "#28774a",
+				green = "#327a2e",
+				purple = "#950095",
+				red = "#be342c",
+				white = "#BBBBBB",
+				yellow = "#917d54",
+				orange = "#a65d37",
+				highlight = "#c2a36b",
+				comment = "#81858c",
+			}
+
+			require("onedarkpro").setup({ colors = custom_colors })
+			vim.cmd("colorscheme onelight")
 		end,
-		-- lazy = true,
 	},
 	{
 		"folke/tokyonight.nvim",
